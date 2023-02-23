e2319378b486f1cba3d30ca8c0e43650d5ead672
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-22 22:23:23 -0700

    Created Delete Client Login (aka Client Logout), tested and working as expected.

--- endpoints/client_login.py
+++ endpoints/client_login.py
@@ -24,3 +24,24 @@ def post_client_login():
             return make_response(jsonify("Something went wrong, please try again."), 500)
     else:
         return make_response(jsonify(result), 500)
+
+# DELETE Client Login Sessions
+@app.delete('/api/client-login')
+def delete_candy():
+    """
+    Expects 1 Argument:
+    Token
+    """
+    required_data = ['token']
+    check_result = check_data(request.json, required_data)
+    if check_result != None:
+        return check_result
+    token = request.json.get('token')
+    result = run_statement("CALL client_logout(?)", [token])
+    if (type(result) == list):
+        if result[0][0] == 1:
+            return make_response(jsonify("Successfully logged out."), 200)
+        elif result[0][0] == 0:
+            return make_response(jsonify("Error, please try logging out again."), 400)
+    else:
+        "There has been an unexpected error."
