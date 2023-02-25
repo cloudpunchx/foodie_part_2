2f0d25ed664023ad678bd7cfb18f741286b3765e
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-25 16:39:01 -0700

    Added detailed Error Handling for Check and UN Constraints for POST Restaurant (sign up). Tested and working as expected.

--- endpoints/restaurant.py
+++ endpoints/restaurant.py
@@ -25,6 +25,43 @@ def get_restaurants():
         return make_response(jsonify(response), 500)
 
 # POST Restaurants
+# @app.post('/api/restaurant')
+# def create_restaurant():
+#     """
+#     Expects 7 Args:
+#     name, address, bio, city, email, password, phoneNum
+#     Optional:
+#     bannerUrl, profileUrl
+#     """
+#     required_data = ['name', 'address', 'bio', 'city', 'email', 'password', 'phoneNum']
+#     check_result = check_data(request.json, required_data)
+#     if check_result != None:
+#         return check_result
+#     name = request.json.get('name')
+#     address = request.json.get('address')
+#     bio = request.json.get('bio')
+#     city = request.json.get('city')
+#     email = request.json.get('email')
+#     password = request.json.get('password')
+#     phone_num = request.json.get('phoneNum')
+#     banner_url = request.json.get('banner_url')
+#     profile_url = request.json.get('profile_url')
+#     salt = bcrypt.gensalt()
+#     hash_result = bcrypt.hashpw(password.encode(), salt)
+#     result = run_statement("CALL create_restaurant_profile(?,?,?,?,?,?,?,?,?)", [name, address, bio, city, email, hash_result, phone_num, banner_url, profile_url])
+#     if (type(result) == list):
+#         if result[0][0] == 1:
+#             return make_response(jsonify("Successfully created profile."), 200)
+#         elif result[0][0] == 0:
+#             return make_response(jsonify("Something went wrong, please try again."), 500)
+#     elif "restaurant_UN_email" in result:
+#         return make_response(jsonify("This email is already in use, please enter another email or click forgot password."), 409)
+#     elif "restaurant_UN_username" in result:
+#         return make_response(jsonify("This username is already in use, please enter another username."), 409)
+#     else:
+#         return make_response(jsonify(result), 500)
+
+# LEAVING OFF SEPARATING INTO 2 PROCEDURES
 @app.post('/api/restaurant')
 def create_restaurant():
     """
@@ -49,17 +86,24 @@ def create_restaurant():
     salt = bcrypt.gensalt()
     hash_result = bcrypt.hashpw(password.encode(), salt)
     result = run_statement("CALL create_restaurant_profile(?,?,?,?,?,?,?,?,?)", [name, address, bio, city, email, hash_result, phone_num, banner_url, profile_url])
-    if (type(result) == list):
-        if result[0][0] == 1:
-            return make_response(jsonify("Successfully created profile."), 200)
+    if "restaurant_UN_email" in result:
+        return make_response(jsonify("Error: This email is already in use, please enter another email."), 422)
+    elif "restaurant_CHECK_city_name" in result:
+        return make_response(jsonify("Error: Chosen City unavailable, choose from approved City list."), 422)
+    elif "restaurant_CHECK_email_format" in result:
+        return make_response(jsonify("Error: Email must be in valid email format."), 422)
+    elif "restaurant_CHECK_phoneNum_format" in result:
+        return make_response(jsonify("Error: Phone Number must be in valid format: xxx-xxx-xxxx"), 422)
+    else:
+        result = run_statement("CALL restaurant_login(?)", [result[0][0]])
+        if (type(result) == list):
+            restaurant_id = result[0][0]
+            token = result[0][1]
+            return make_response(jsonify(f"Welcome User {restaurant_id}, login successful."), 200)
         elif result[0][0] == 0:
             return make_response(jsonify("Something went wrong, please try again."), 500)
-    elif "restaurant_UN_email" in result:
-        return make_response(jsonify("This email is already in use, please enter another email or click forgot password."), 409)
-    elif "restaurant_UN_username" in result:
-        return make_response(jsonify("This username is already in use, please enter another username."), 409)
-    else:
-        return make_response(jsonify(result), 500)
+        else:
+            return make_response(jsonify(result), 500)
 
 # PATCH Restaurant Profile
 @app.patch('/api/restaurant')
