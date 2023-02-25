e5c3394a71a80b0b7a5099965c5fa8924474b8a3
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-24 18:14:19 -0700

    I accidentally deleted 'get_client_profile' procedure in db, re-did and tested. Working as expected.

--- endpoints/client.py
+++ endpoints/client.py
@@ -5,6 +5,27 @@ from helpers.dbhelpers import run_statement
 from helpers.helpers import check_data
 
 # GET Client Profile   
+# @app.get('/api/client')
+# def get_client_profile():
+#     """
+#     Expects 1 Arg:
+#     token
+#     """
+#     required_data = ['token']
+#     check_result = check_data(request.json, required_data)
+#     if check_result != None:
+#         return check_result
+#     token = request.json.get('token')
+#     keys = ["clientId", "username", "firstName", "lastName", "email", "pictureUrl", "createdAt"]
+#     result = run_statement('CALL get_client_profile(?)', [token])
+#     if(type(result) == list):
+#         for client in result:
+#             zipped = zip(keys, client)
+#             client = (dict(zipped))
+#             return make_response(jsonify(client), 200)
+#     else:
+#         return make_response(jsonify('Something went wrong, please try again.'), 500)
+
 @app.get('/api/client')
 def get_client_profile():
     """
@@ -19,46 +40,16 @@ def get_client_profile():
     keys = ["clientId", "username", "firstName", "lastName", "email", "pictureUrl", "createdAt"]
     result = run_statement('CALL get_client_profile(?)', [token])
     if(type(result) == list):
+        if result == []:
+            return make_response(jsonify('Invalid token in request.'), 500)
         for client in result:
             zipped = zip(keys, client)
             client = (dict(zipped))
-        return make_response(jsonify(client), 200)
+            return make_response(jsonify(client), 200)
     else:
-        return make_response(jsonify(result), 500)
+        return make_response(jsonify('Something went wrong, please try again.'), 500)
 
-# Post Client Profile, (Client Sign Up) then logs user session and created token in 1 stored procedure that calls another procedure.
-# @app.post('/api/client')
-# def post_client():
-#     """
-#     Expects 5 Args:
-#     username, firstName, lastName, email, password
-#     1 Optional:
-#     pictureUrl
-#     """
-#     required_data = ['username', 'firstName', 'lastName', 'email', 'password']
-#     check_result = check_data(request.json, required_data)
-#     if check_result != None:
-#         return check_result
-#     username = request.json.get('username')
-#     first_name = request.json.get('firstName')
-#     last_name = request.json.get('lastName')
-#     email = request.json.get('email')
-#     password = request.json.get('password')
-#     pictureUrl = request.json.get('pictureUrl')
-#     result = run_statement("CALL create_client_profile(?,?,?,?,?,?)", [username, first_name, last_name, email, password, pictureUrl])
-#     if (type(result) == list):
-#         if result[0][0] == 1:
-#             return make_response(jsonify("Successfully created profile."), 200)
-#         elif result[0][0] == 0:
-#             return make_response(jsonify("Something went wrong, please try again."), 500)
-#     elif "client_UN_email" in result:
-#         return make_response(jsonify("This email is already in use, please enter another email or click forgot password."), 409)
-#     elif "client_UN_username" in result:
-#         return make_response(jsonify("This username is already in use, please enter another username."), 409)
-#     else:
-#         return make_response(jsonify(result), 500)
-
-# Implementing bcrypt hash and salt
+# POST Client Profile, (Client Sign Up) then logs user session and created token in 1 stored procedure that calls another procedure.
 @app.post('/api/client')
 def post_client():
     """
@@ -92,4 +83,53 @@ def post_client():
     else:
         return make_response(jsonify(result), 500)
 
+# PATCH Client Profile
+# I intentionally left email out because you can't change it anyways
+# LEAVING OFF WITH THIS NOT WORKING
+@app.patch('/api/client')
+def patch_client():
+    """
+    Expects 1 Arg:
+    token
+    Optional:
+    username, firstName, lastName, password, pictureUrl
+    """
+    required_data = ['token']
+    check_result = check_data(request.json, required_data)
+    if check_result != None:
+        return check_result
+    token = request.json.get('token')
+    username = request.json.get('username')
+    first_name = request.json.get('firstName')
+    last_name = request.json.get('lastName')
+    password = request.json.get('password')
+    pictureUrl = request.json.get('pictureUrl')
+    result = run_statement('CALL edit_client_profile(?,?,?,?,?,?)', [token, username, first_name, last_name, password, pictureUrl])
+    if (type(result) == list):
+        if result[0][0] == 1:
+            return make_response(jsonify("Successfully edited profile."), 200)
+        elif result[0][0] == 0:
+            return make_response(jsonify("Something went wrong, please try again."), 500)
+    else:
+        return make_response(jsonify(result), 500)
 
+# DELETE Client Profile
+@app.delete('/api/client')
+def delete_client():
+    """
+    Expects 1 Arg:
+    token
+    """
+    required_data = ['token']
+    check_result = check_data(request.json, required_data)
+    if check_result != None:
+        return check_result
+    token = request.json.get('token')
+    result = run_statement("CALL delete_client_profile(?)", [token])
+    if (type(result) == list):
+        if result[0][0] == 1:
+            return make_response(jsonify("Successfully deleted account."), 200)
+        elif result[0][0] == 0:
+            return make_response(jsonify("Error, please try again."), 400)
+    else:
+        "There has been an unexpected error."
--- endpoints/client_login.py
+++ endpoints/client_login.py
@@ -1,29 +1,40 @@
 from app import app
+import bcrypt
 from flask import make_response, jsonify, request
 from helpers.dbhelpers import run_statement
 from helpers.helpers import check_data
 
+# MOVING ON - COME BACK AND ADD PASSWORD ENCRYPTING, FOLLOW STEPS IN NOTES. 
 # POST Client Login Sessions, completed through 1 stored procedure
+# @app.post('/api/client-login')
+# def post_client_login():
+#     """
+#     Expects 2 Args
+#     Email, Password
+#     """
+#     required_data = ['email', 'password']
+#     check_result = check_data(request.json, required_data)
+#     if check_result != None:
+#         return check_result
+#     email = request.json.get('email')
+#     password = request.json.get('password')
+#     result = run_statement("CALL client_login(?,?)", [email, password])
+#     if (type(result) == list):
+#         if result[0][0] == 1:
+#             return make_response(jsonify("Successfully logged in."), 200)
+#         elif result[0][0] == 0:
+#             return make_response(jsonify("Something went wrong, please try again."), 500)
+#     else:
+#         return make_response(jsonify(result), 500)
+
 @app.post('/api/client-login')
 def post_client_login():
     """
     Expects 2 Args
     Email, Password
     """
-    required_data = ['email', 'password']
-    check_result = check_data(request.json, required_data)
-    if check_result != None:
-        return check_result
-    email = request.json.get('email')
-    password = request.json.get('password')
-    result = run_statement("CALL client_login(?,?)", [email, password])
-    if (type(result) == list):
-        if result[0][0] == 1:
-            return make_response(jsonify("Successfully logged in."), 200)
-        elif result[0][0] == 0:
-            return make_response(jsonify("Something went wrong, please try again."), 500)
-    else:
-        return make_response(jsonify(result), 500)
+    pass
+
 
 # DELETE Client Login Sessions, completed through 1 stored procedure
 @app.delete('/api/client-login')
